16496ebbe24c4fdf7fea27cc782dfc5b18cf758d
Malerie Earle <141525464+malerie-earle@users.noreply.github.com>
2024-02-26 21:11:55 -0330

    Delete NN directory

--- NN/insert scripts/public._vendorAccount_.sql
+++
@@ -1,7 +0,0 @@
-insert into public."vendorAccount" (account_id, person_id, username, password) values (1, 11, 'lriping0', 'oH1}%@V@=T');
-insert into public."vendorAccount" (account_id, person_id, username, password) values (2, 11, 'kskryne1', 'iL9)e)nRm_Na)>');
-insert into public."vendorAccount" (account_id, person_id, username, password) values (3, 13, 'csparling2', 'kC2\7mI7');
-insert into public."vendorAccount" (account_id, person_id, username, password) values (4, 15, 'kserchwell3', 'kE4@cEk>|S/kx?');
-insert into public."vendorAccount" (account_id, person_id, username, password) values (5, 12, 'xpetranek4', 'gT5!qyeEbnP');
-insert into public."vendorAccount" (account_id, person_id, username, password) values (6, 11, 'lropartz5', 'nB0#SLz|LsU');
-insert into public."vendorAccount" (account_id, person_id, username, password) values (7, 15, 'zclerke6', 'mQ3\4ov>QV');
--- NN/insert scripts/vendor.sql
+++
@@ -1,7 +0,0 @@
-insert into public."vendor" (vendor_id, person_id, vendor_name) values (1, 9, 'Twimbo');
-insert into public."vendor" (vendor_id, person_id, vendor_name) values (2, 12, 'Photobug');
-insert into public."vendor" (vendor_id, person_id, vendor_name) values (3, 10, 'Skippad');
-insert into public."vendor" (vendor_id, person_id, vendor_name) values (4, 13, 'Skyba');
-insert into public."vendor" (vendor_id, person_id, vendor_name) values (5, 15, 'Edgewire');
-insert into public."vendor" (vendor_id, person_id, vendor_name) values (6, 14, 'Topicshots');
-insert into public."vendor" (vendor_id, person_id, vendor_name) values (7, 11, 'Riffpedia');
